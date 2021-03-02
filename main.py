13c2a1139f126370b549b6dcca7615c6fc0ee2c4
lersveen <7195448+lersveen@users.noreply.github.com>
2021-03-02 16:59:03 +0100

    MVP
    - Should now work, probably

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
+import sys
 import json
 import requests
+import xmltodict
 
-imdb_id = '123'
-api_key = '123'
-sonarr_host = 'localhost'
+try:
+    imdb_id = str(sys.argv[1])
+    api_key = str(sys.argv[2])
+except Exception:
+    raise ValueError('Missing arguments – need both: <IMDB id> <Sonarr api key>')
+    sys.exit()
+
+sonarr_host = 'localhost:8989/sonarr'
+user_agent = 'Sonarr-show-adder'
 
 
-def send_to_sonarr():
+def get_tvdb_id(imdb_id):
+    url = f'https://thetvdb.com/api/GetSeriesByRemoteID.php?imdbid={imdb_id}'
+
+    try:
+        r = requests.get(url=url)
+
+        r.raise_for_status()
+
+        print('Succeeded getting info from tvdb')
+
+        response_dict = xmltodict.parse(r.content)
+
+        return response_dict['Data']['Series'].get('seriesid')
+
+    except Exception as e:
+        print('Failed getting info from tvdb')
+        print(e)
+        return None
+
+
+def lookup_series(tvdb_id):
+    headers = {
+        'User-Agent': user_agent,
+        'X-Api-Key': api_key,
+        'Content-Type': 'application/json'
+        }
+
+    url = 'http://{SONARR_HOST}/api/series/lookup?term=tvdb:{tvdb_id}'
+
+    try:
+        r = requests.get(url=url, headers=headers)
+
+        r.raise_for_status()
+
+        print('Succeeded looking up series')
+
+        return r.json()
+    except Exception:
+        print('Failed looking up series')
+        return None
+
+
+def send_to_sonarr(series_info):
     payload = {
         'monitored': True,
-        'imdbId': imdb_id,
-        'title': 'The Jinx: The Life and Deaths of Robert Durst',
-        'titleSlug': 'the-jinx-the-life-and-deaths-of-robert-durst',
+        'tvdbId': series_info[0].get('tvdbId'),
+        'title': series_info[0].get('title'),
+        'profileId': series_info[0].get('profileId'),
+        'titleSlug': series_info[0].get('titleSlug'),
         'seasonFolder': True,
+        'seasons': series_info[0].get('seasons'),
+        'images': series_info[0].get('images'),
         'qualityProfileId': '1',
         'rootFolderPath': '/home/TV Shows/',
-        'seasons':
-            [
-                {
-                    'monitored': True, 'seasonNumber': 1
-                }
-            ],
-            'addOptions': {
-                'searchForMissingEpisodes': True,
-                'ignoreEpisodesWithFiles': False
-                }
+        'addOptions': {
+            'searchForMissingEpisodes': True,
+            'ignoreEpisodesWithFiles': False
+            }
         }
 
     headers = {
-        'User-Agent': "Show adder",
+        'User-Agent': user_agent,
         'X-Api-Key': api_key,
         'Content-Type': 'application/json'
         }
@@ -48,6 +95,7 @@ def send_to_sonarr():
         return None
 
 
-result = send_to_sonarr()
-
-print(result)
+if __name__ == "__main__":
+    tvdb_id = get_tvdb_id(imdb_id)
+    series_info = lookup_series(tvdb_id)
+    send_to_sonarr(series_info)
